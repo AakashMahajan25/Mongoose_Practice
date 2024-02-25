f4a4c45d4a79c5909bcddfd9c0b6d546dd418048
aakash <mahajanaakash108@gmail.com>
2024-02-25 11:36:15 +0530

    Mongoose connection working

---
+++ index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+try {
+    
+    await mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB', error.message);
+    });
+
+
+} catch (error) {
+    
+}
